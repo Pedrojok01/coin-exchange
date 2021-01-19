9a16bc98bfcc6b2df1a0ce436b9e849acb7fb0c0
Pedrojok01 <pedrojok@pm.me>
2021-01-19 16:32:35 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Coin from './components/Coin/Coin';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">
+          Coin Exchange
+        </h1>
+      </header>
+
+      <table className="coin-table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Ticker</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        <Coin name="Bitcoin" ticker="BTC" price={35000.99}/>
+        <Coin name="Ethereum" ticker="ETH" price={1299.99}/>
+        <Coin name="Tether" ticker="USDT" price={1}/>
+        <Coin name="Polkadot" ticker="DOT" price={15.99}/>
+        <Coin name="Ripple" ticker="XRP" price={0.2}/>
+
+      </tbody>
+    </table>
+    </div>
+  );
+}
+
+export default App;
