0b88045de1e11a02fb8deb7ffbd21f0c1edcbe33
Pedrojok01 <pedrojok@pm.me>
2021-01-25 11:29:26 +0700

    lifting state up

--- src/components/Coin/Coin.jsx
+++ src/components/Coin/Coin.jsx
@@ -10,21 +10,12 @@ const Td = styled.td`
 export default class Coin extends Component {
     constructor(props){
         super(props);
-        this.state = {
-            price: this.props.price
-        }
         this.handleClick = this.handleClick.bind(this);
     }
 
-    handleClick(event){
+    handleClick(event) {
         event.preventDefault();
-        
-        const randomPercentage = 0.995 + Math.random() * 0.01;
-        this.setState( function(oldState) {
-            return {
-                price: oldState.price * randomPercentage
-            };
-        });
+        this.props.handleRefresh(this.props.ticker);
     }
 
     render() {
@@ -32,7 +23,7 @@ export default class Coin extends Component {
           <tr>
             <Td>{this.props.name}</Td>
             <Td>{this.props.ticker}</Td>
-            <Td>${this.state.price}</Td>
+            <Td>${this.props.price}</Td>
             <Td>
                 <form action="#" method="POST">
                     <button onClick={this.handleClick}>Refresh</button>
--- src/components/CoinList/CoinList.jsx
+++ src/components/CoinList/CoinList.jsx
@@ -23,7 +23,11 @@ export default class CoinList extends Component {
             <tbody>
               {
                 this.props.coinData.map( ({name, ticker, price}) =>
-                    <Coin key={ticker} name={name} ticker={ticker} price={price} />
+                    <Coin key={ticker}
+                        handleRefresh={this.props.handleRefresh}
+                        name={name}
+                        ticker={ticker}
+                        price={price}/>
                 )
               }
             </tbody>
